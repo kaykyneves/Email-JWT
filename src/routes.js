a91f47fa1762b6d852d6cf9e67f3538c446e660a
kayky <kaykyneves42@gmail.com>
2024-01-15 15:54:50 -0300

    feat (login) Login iimplemented and bug fixes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
 const { createUser } = require("./controller/userController");
+const {loginUser} = require("./controller/loginUser")
 const route = express.Router();
 
 route.post("/user", createUser);
+route.post("/login", loginUser);
+
 
 module.exports = route;
